5232f04efc9ed20d4418c2bbdb5ec30092ddcb14
AYE MJ <ayemj.work@gmail.com>
2018-03-22 23:40:59 +0530

    Try 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,8 @@ import { Routes, RouterModule, CanActivate } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ShowOnDirtyErrorStateMatcher, ErrorStateMatcher, MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule } from '@angular/material';
 import { AppComponent } from './app.component';
-import { MasterComponent } from './master/master.component';
 import { TimeTableComponent } from './time-table/time-table.component';
-import { ClassSubjectDetailsComponent } from './master/class-subject-details/class-subject-details.component';
-import { TeacherDetailsComponent } from './master/teacher-details/teacher-details.component';
-import { LeavesDetailsComponent } from './master/leaves-details/leaves-details.component';
 import { LoginComponent } from './login/login.component';
-import { AddEditDetailsComponent } from './master/class-subject-details/add-edit-details/add-edit-details.component';
-import { ViewDetailsComponent } from './master/class-subject-details/view-details/view-details.component';
 import { StudentTimeTableComponent } from './time-table/student-time-table/student-time-table.component';
 import { TeacherTimeTableComponent } from './time-table/teacher-time-table/teacher-time-table.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -43,14 +37,11 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    MasterComponent,
+    
     TimeTableComponent,
-    ClassSubjectDetailsComponent,
-    TeacherDetailsComponent,
-    LeavesDetailsComponent,
+    
       LoginComponent,
-     AddEditDetailsComponent,
-     ViewDetailsComponent,
+     
      StudentTimeTableComponent,
      TeacherTimeTableComponent,
      DashboardComponent,
